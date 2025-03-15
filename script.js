b1dcb153e2b35357fa68b641c4210b38c534b8e3
Pankaj Maurya <100466858+Pankaj352@users.noreply.github.com>
2025-03-15 23:38:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+document.querySelector(".btn").addEventListener("click", function() {
+    let searchBox = document.querySelector(".search");
+    let inputField = document.querySelector(".input");
+    searchBox.classList.toggle("active");
+    inputField.focus();
+});
